a7c65ccfa14eab0669b7f0800cdc5a858d909d22
Sascha Lisson <sl@q60.de>
2025-04-21 11:40:58 +0200

    ci: publish images with skaffold

--- helm/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-cd "$(dirname "$0")"
-
-helm install dev ./modelix -f dev.yaml
--- helm/upgrade.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-cd "$(dirname "$0")"
-
-helm upgrade dev ./modelix -f dev.yaml
